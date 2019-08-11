dc78231195ccca0d2ad998e416376fb36d071fc9
sunlongyue@foxmail.com <sunlongyue@foxmail.com>
2019-08-11 17:50:39 +0800

    提交云配置文件

---
+++ hello-spring-cloud-config/src/main/java/com/funtl/hello/spring/cloud/config/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.funtl.hello.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: domain
+ * @create: 2019/8/11 17:48
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
